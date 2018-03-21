c9027110ffd3372acfb7ac37c7eadced9d906cea
Alexander Spanos <alexisspa9@gmail.com>
2018-03-21 20:08:54 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,14 @@
 def unsafe?(speed)
-	speed == 'unsafe' ? true : false
+	if speed <40 && speed > 60
+		return true
+	else
+		return false
+	end
 
 end
 
 
 
 def not_safe?(speed)
-	
+	speed < 40 && speed > 60 ? true : false
 end
-	
-
-
